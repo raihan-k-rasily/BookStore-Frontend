54db83be0904ac0618254c27deb10943e5024eb8
raihan-k-rasily <raihankrasily@gmail.com>
2026-01-04 12:13:44 +0530

    Day 9

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
       <ContextShare>
         <App />
       </ContextShare>
-      </GoogleOAuthProvider>;
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
